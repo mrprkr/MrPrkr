d69b4de245d94e2d50ad4292327783068ea6380e
Michael Parker <parker_michael@me.com>
2015-02-15 15:22:53 +1100

    masonry working correctly

--- build/js/angular-app.js
+++ build/js/angular-app.js
@@ -1,52 +1,83 @@
 
 //Define angular app
-var app = angular.module('app', ['templatescache', 'ngRoute', 'ngAnimate', 'wu.masonry'])
+var app = angular.module('app', ['templatescache', 'ngRoute', 'ngAnimate', 'wu.masonry', 'angular-images-loaded'])
 
 //configure application routes, 
 //note: this is using gulp-angular-template-cache so only template names are needed
 app.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider
+	function($routeProvider, $locationProvider) {
+		$routeProvider
 
-      .when('/', {
-        templateUrl: 'home.html',
-        controller: 'main-controller'
-      })
+			.when('/', {
+				templateUrl: 'home.html',
+				controller: 'main-controller'
+			})
 
-      .when('/view1', {
-        templateUrl: 'view1.html',
-        controller: 'main-controller'
-      })
+			.when('/view1', {
+				templateUrl: 'view1.html',
+				controller: 'main-controller'
+			})
 
-      .when('/view2', {
-        templateUrl: 'view2.html',
-        controller: 'main-controller'
-      })
+			.when('/view2', {
+				templateUrl: 'view2.html',
+				controller: 'main-controller'
+			})
 
-      .when('/view3/:id', {
-        templateUrl: 'view3.html',
-        controller: 'view3-controller'
-      })
+			.when('/view3/:id', {
+				templateUrl: 'view3.html',
+				controller: 'view3-controller'
+			})
 
-      .when('/view3', {
-        templateUrl: 'view3.html',
-        controller: 'view3-controller'
-      })
+			.when('/view3', {
+				templateUrl: 'view3.html',
+				controller: 'view3-controller'
+			})
 
-      .otherwise({
-      	redirectTo: '/#'
-      });
+			.otherwise({
+				redirectTo: '/#'
+			});
 
-    $locationProvider.html5Mode(false);
+		$locationProvider.html5Mode(false);
 }])
 
 //The main controller
-app.controller('main-controller', function($scope){
+app.controller('main-controller', function($scope, $window, $timeout, $rootScope){
 	$scope.projects = projects;
-  
+	$scope.imagesAreLoaded = false;
+	
+	/* Reloads masonry after window resize
+	delayed to use CSS animation
+	calls passy's reload() directive using rootScope*/
+	$scope.reloadMasonry = function() {  //calls the function
+		$timeout(function () {
+			$rootScope.$broadcast('masonry.reload');
+		}, 600);
+	}
+
+	// var w = angular.element($window);   //watches the window
+	// 	w.bind('resize', function () {
+	// 	$scope.reloadMasonry();
+	// });
+		
+
+	//detect when images are still loading
+	$scope.imgLoadedEvents = {
+				always: function(instance) {
+						// Do stuff
+				},
+
+				done: function(instance) {
+						$scope.imagesAreLoaded = true;
+				},
+
+				fail: function(instance) {
+						// Do stuff
+				}
+
+		}
 
 });
 
 app.controller('view3-controller', function($scope, $routeParams){
-  $scope.routeId = $routeParams.id;
+	$scope.routeId = $routeParams.id;
 })
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-var staticPath = path.resolve(__dirname, './public/');
+var staticPath = path.resolve(__dirname, './public');
 app.use(express.static(staticPath));
 
 app.listen(8080, function() {
